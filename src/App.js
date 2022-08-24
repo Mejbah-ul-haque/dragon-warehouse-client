ae2af27d876d7ae554575d8ec40c7e9d7a42117c
Mejbah <mejba.s@gmail.com>
2022-08-25 03:30:38 +0600

    Route set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { Routes } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home/Home";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
 
@@ -7,7 +9,11 @@ function App() {
 	return (
 		<div>
 			<Header></Header>
-   
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
       <Footer></Footer>
 		</div>
 	);
