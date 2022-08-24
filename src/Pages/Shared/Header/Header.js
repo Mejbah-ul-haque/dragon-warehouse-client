3023de514f182a49f2e2239693efd98092afef34
Mejbah <mejba.s@gmail.com>
2022-08-25 03:37:26 +0600

    navbar changes

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,6 +1,28 @@
 import React from "react";
 
 const Header = () => {
+	const manuItems = (
+		<>
+			<li>
+				<a>Manage Items</a>
+			</li>
+			<li>
+				<a>Add Item</a>
+			</li>
+			<li>
+				<a>My Items</a>
+			</li>
+			<li>
+				<a>Blog</a>
+			</li>
+			<li>
+				<a>About</a>
+			</li>
+			<li>
+				<a>Login</a>
+			</li>
+		</>
+	);
 	return (
 		<div className="navbar bg-base-100">
 			<div className="navbar-start">
@@ -25,80 +47,14 @@ const Header = () => {
 						tabIndex="0"
 						className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
 					>
-						<li>
-							<a>Item 1</a>
-						</li>
-						<li tabIndex="0">
-							<a className="justify-between">
-								Parent
-								<svg
-									className="fill-current"
-									xmlns="http://www.w3.org/2000/svg"
-									width="24"
-									height="24"
-									viewBox="0 0 24 24"
-								>
-									<path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-								</svg>
-							</a>
-							<ul className="p-2">
-								<li>
-									<a>Submenu 1</a>
-								</li>
-								<li>
-									<a>Submenu 2</a>
-								</li>
-							</ul>
-						</li>
-						<li>
-							<a>Item 3</a>
-						</li>
+						{manuItems}
 					</ul>
 				</div>
 				<a className="btn btn-ghost normal-case text-xl">Dragon Warehouse</a>
 			</div>
 			<div className="navbar-center hidden lg:flex">
 				<ul className="menu menu-horizontal p-0">
-					<li>
-						<a>Manage Items</a>
-					</li>
-					<li>
-						<a>Add Item</a>
-					</li>
-					<li>
-						<a>My Items</a>
-					</li>
-					<li>
-						<a>Blog</a>
-					</li>
-					<li tabIndex="0">
-						<a>
-							Categories
-							<svg
-								className="fill-current"
-								xmlns="http://www.w3.org/2000/svg"
-								width="20"
-								height="20"
-								viewBox="0 0 24 24"
-							>
-								<path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-							</svg>
-						</a>
-						<ul className="p-2">
-							<li>
-								<a>Submenu 1</a>
-							</li>
-							<li>
-								<a>Submenu 2</a>
-							</li>
-						</ul>
-					</li>
-					<li>
-						<a>About</a>
-					</li>
-					<li>
-						<a>Login</a>
-					</li>
+					{manuItems}
 				</ul>
 			</div>
 		</div>
