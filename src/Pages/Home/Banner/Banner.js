54380bc9ec9c9a4e79edcf4fee393802d3be06de
Mejbah <mejba.s@gmail.com>
2022-08-25 11:49:48 +0600

    Caruesel adde

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const Banner = () => {
-  return (
-    <div>
-      <h2>This is Banner</h2>
-    </div>
-  );
+	return (
+		<div className="bg-slate-400 py-10">
+			<div class="container mx-auto carousel carousel-center rounded-box">
+				<div class="carousel-item">
+					<img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+				</div>
+				<div class="carousel-item">
+					<img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+				</div>
+				<div class="carousel-item">
+					<img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+				</div>
+				<div class="carousel-item">
+					<img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+				</div>
+				<div class="carousel-item">
+					<img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+				</div>
+				<div class="carousel-item">
+					<img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+				</div>
+				<div class="carousel-item">
+					<img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
