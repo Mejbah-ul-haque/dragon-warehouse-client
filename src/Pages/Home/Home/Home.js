0e09cd56d75360128c2022253152baaa88a23714
Mejbah <mejba.s@gmail.com>
2022-08-26 15:50:51 +0600

    updated

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Items from '../Items/Items';
+import Special1 from '../Special1/Special1';
 
 const Home = () => {
   return (
     <div className="bg-[#ededed]">
     <Banner></Banner>
     <Items></Items>
+    <Special1></Special1>
     </div>
       
  
--- src/Pages/Home/Special1/Special1.js
+++ src/Pages/Home/Special1/Special1.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import extra1 from '../../../Images/Extra/extra1.jpg';
-import extra2 from '../../../Images/Extra/extra2.jpg';
-import item13 from '../../../Images/Items/item13.jpg';
-import item18 from '../../../Images/Items/item18.jpg';
-import item19 from '../../../Images/Items/item19.jpg';
-import item20 from '../../../Images/Items/item20.jpg';
+import extra1 from '../../../assets/Extra/extra1.jpg';
+import extra2 from '../../../assets/Extra/extra2.jpg';
+import item13 from '../../../assets/Items/item13.jpg';
+import item18 from '../../../assets/Items/item18.jpg';
+import item19 from '../../../assets/Items/item19.jpg';
+import item20 from '../../../assets/Items/item20.jpg';
 
 const Special1 = () => {
     return (
-        <div className='container mt-5'>
-            <p style={{ backgroundColor: '#e2e2e2', marginTop: '70px' }} className='w-50 d-block mx-auto'><span style={{ color: 'white', borderRadius: '10px' }} className='btn fw-bold d-block mx-auto mt-4 mb-3 px-5 text-dark text-uppercase'>Extra Section one</span></p>
+        <div className='container mx-auto mt-24'>
+            
 
-            <div className="row g-2">
-                <div className="col-12 col-md-6">
-                    <img className='img-fluid w-100' src={extra1} alt="" />
+            <div className="grid grid-cols-4 gap-4">
+                <div className="col-span-2">
+                    <img className='w-full border border-[#e6e6e6] opacity-90 hover:opacity-100' src={extra1} alt="" />
                 </div>
-                <div className="col-6 col-md-3">
-                    <img className='img-fluid w-100' src={item13} alt="" />
+                <div className="">
+                    <img className='w-full border border-[#e6e6e6] opacity-90 hover:opacity-100' src={item13} alt="" />
                 </div>
-                <div className="col-6 col-md-3">
-                    <img className='img-fluid w-100' src={item18} alt="" />
+                <div className="">
+                    <img className='w-full border border-[#e6e6e6] opacity-90 hover:opacity-100' src={item18} alt="" />
                 </div>
-                <div className="col-6 col-md-3">
-                    <img className='img-fluid w-100' src={item19} alt="" />
+                <div className="">
+                    <img className='w-full border border-[#e6e6e6] opacity-90 hover:opacity-100' src={item19} alt="" />
                 </div>
-                <div className="col-6 col-md-3">
-                    <img className='img-fluid w-100' src={item20} alt="" />
+                <div className="">
+                    <img className='w-full border border-[#e6e6e6] opacity-90 hover:opacity-100' src={item20} alt="" />
                 </div>
-                <div className="col-12 col-md-6">
-                    <img className='img-fluid w-100' src={extra2} alt="" />
+                <div className="col-span-2">
+                    <img className='w-full border border-[#e6e6e6] opacity-90 hover:opacity-100' src={extra2} alt="" />
                 </div>
             </div>
         </div>
