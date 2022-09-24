3d9914d7538278ae3b4b34e43e7366dd31fb43e4
Mejbah <mejba.s@gmail.com>
2022-09-25 01:32:09 +0600

    update

--- src/Pages/AddItem/AddItem.js
+++ src/Pages/AddItem/AddItem.js
@@ -1,214 +1,96 @@
-import React from 'react';
+import React from "react";
 import { useForm } from "react-hook-form";
-import { useQuery } from "react-query";
-import { toast } from "react-toastify";
-import Loading from "../Shared/Loading/Loading";
-
+import axios from "axios";
 const AddItem = () => {
-  const {
-		register,
-		formState: { errors },
-		handleSubmit,
-    reset
-	} = useForm();
+	const { register, handleSubmit } = useForm();
 
-	const { data: services, isLoading } = useQuery("services", () =>
-		fetch("http://localhost:4000/service").then((res) => res.json())
-	);
-  
-  const imgStorageKey = "d2ed86d276998da67966121ea46a21d3";
-
-	const onSubmit = async (data) => {
-		const image = data.image[0];
-    const formData = new FormData();
-    formData.append('image', image);
-    const url = `https://api.imgbb.com/1/upload?key=${imgStorageKey}`;
-    fetch(url, {
-      method: "POST",
-      body: formData
-    })
-    .then(res => res.json())
-    .then(result => {
-      if(result.success) {
-        const img = result.data.url;
-        const product = {
-          name: data.name,
-          price: data.price,
-          description: data.description,
-          minQuantity: data.minQuantity,
-          availableQuantity: data.availableQuantity,
-          img: img
-        }
-        fetch('http://localhost:4000/service', {
-          method: "POST",
-          headers: {
-            'content-type': 'application/json',
-            authorization: `Bearer ${localStorage.getItem('accessToken')}`
-          },
-          body: JSON.stringify(product)
-        })
-        .then(res => res.json())
-        .then(inserted =>{
-          if(inserted.insertedId){
-            toast.success('Product added successfully')
-            reset();
-          }
-          else{
-            toast.error('Failed to add product');
-          }
-        })
-      }
-    })
+	const onSubmit = (data) => {
+		console.log(data);
+		axios.post("/service", data).then(res=>console.log(res.data))
+		
+		// const url = `http://localhost:4000/service`;
+		// fetch(url, {
+		// 	method: "POST",
+		// 	headers: {
+		// 		"content-type": "application/json",
+		// 	},
+		// 	body: JSON.stringify(data),
+		// })
+		// 	.then((res) => res.json())
+		// 	.then((result) => {
+		// 		console.log(result);
+		// 	});
 	};
 
-	if (isLoading) {
-		return <Loading></Loading>;
-	}
-
 	return (
-		<div>
-			<h3 className="text-2xl my-3">Add a new Product</h3>
-			<div className="flex justify-center items-center mt-10">
-				<div className="card w-96 bg-base-100 shadow-xl">
-					<div className="card-body">
-						<h2 className="text-center text-2xl">Product Information</h2>
-						<form onSubmit={handleSubmit(onSubmit)}>
-							<div className="form-control w-full max-w-xs">
-								<label className="label">
-									<span className="label-text">Product's Name</span>
-								</label>
-								<select {...register("name")} className="select select-primary w-full max-w-xs">
-                  {
-                    services.map(service =><option
-                    key={service._id}
-                    value={service.name}
-                    >{service.name}</option>)
-                  }
-								</select>
-							</div>
-							<div className="form-control w-full max-w-xs">
-								<label className="label">
-									<span className="label-text">Price</span>
-								</label>
-								<input
-									type="number"
-									placeholder="Product Price"
-									className="input input-bordered w-full max-w-xs"
-									{...register("price", {
-										required: {
-											value: true,
-											message: "Product's price is Required",
-										},
-									})}
-								/>
-								<label className="label">
-									{errors.price?.type === "required" && (
-										<span className="label-text-alt text-red-500">
-											{errors.price.message}
-										</span>
-									)}
-								</label>
-							</div>
-							<div className="form-control w-full max-w-xs">
-								<label className="label">
-									<span className="label-text">Description</span>
-								</label>
-								<input
-									type="textarea"
-									placeholder="Product Description"
-									className="input input-bordered w-full max-w-xs"
-									{...register("description", {
-										required: {
-											value: true,
-											message: "Product's description is Required",
-										},
-									})}
-								/>
-								<label className="label">
-									{errors.description?.type === "required" && (
-										<span className="label-text-alt text-red-500">
-											{errors.description.message}
-										</span>
-									)}
-								</label>
-							</div>
-							<div className="form-control w-full max-w-xs">
-								<label className="label">
-									<span className="label-text">Minimum Quantity</span>
-								</label>
-								<input
-									type="number"
-									placeholder="Minimum Quantity"
-									className="input input-bordered w-full max-w-xs"
-									{...register("minQuantity", {
-										required: {
-											value: true,
-											message: "Minimum Quantity is Required",
-										},
-									})}
-								/>
-								<label className="label">
-									{errors.minQuantity?.type === "required" && (
-										<span className="label-text-alt text-red-500">
-											{errors.minQuantity.message}
-										</span>
-									)}
-								</label>
-							</div>
-							<div className="form-control w-full max-w-xs">
-								<label className="label">
-									<span className="label-text">Available Quantity</span>
-								</label>
-								<input
-									type="number"
-									placeholder="Available Quantity"
-									className="input input-bordered w-full max-w-xs"
-									{...register("availableQuantity", {
-										required: {
-											value: true,
-											message: "Available Quantity is Required",
-										},
-									})}
-								/>
-								<label className="label">
-									{errors.availableQuantity?.type === "required" && (
-										<span className="label-text-alt text-red-500">
-											{errors.availableQuantity.message}
-										</span>
-									)}
-								</label>
-							</div>
-							<div className="form-control w-full max-w-xs">
-								<label className="label">
-									<span className="label-text">Product's Image</span>
-								</label>
-								<input
-									type="file"
-									className="input input-bordered w-full max-w-xs"
-									{...register("image", {
-										required: {
-											value: true,
-											message: "Image is Required",
-										},
-									})}
-								/>
-                <label className="label">
-									{errors.image?.type === "required" && (
-										<span className="label-text-alt text-red-500">
-											{errors.image.message}
-										</span>
-									)}
-								</label>
-							</div>
-
+		<div className="flex h-screen justify-center items-center">
+			<div className="card w-96 bg-base-100 shadow-xl">
+				<div className="card-body">
+					<h2 className="text-center text-2xl font-bold">Please add a Item</h2>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								<span className="label-text">Name</span>
+							</label>
 							<input
-								className="btn w-full max-w-xs"
-								type="submit"
-								value="Add"
+								type="text"
+								placeholder="Name"
+								className="input input-bordered w-full max-w-xs"
+								{...register("name", { required: true, maxLength: 20 })}
 							/>
-						</form>
-					</div>
-				</div>
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								<span className="label-text">Description</span>
+							</label>
+							<textarea
+								type="text"
+								placeholder="Description"
+								className="input input-bordered w-full max-w-xs"
+								{...register("description")}
+							/>
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								<span className="label-text">Price</span>
+							</label>
+							<input
+								type="number"
+								placeholder="Price"
+								className="input input-bordered w-full max-w-xs"
+								{...register("price")}
+							/>
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								<span className="label-text">Qty</span>
+							</label>
+							<input
+								type="number"
+								placeholder="Quantity"
+								className="input input-bordered w-full max-w-xs"
+								{...register("quantity")}
+							/>
+						</div>
+						<div className="form-control w-full max-w-xs">
+							<label className="label">
+								<span className="label-text">Photo URL</span>
+							</label>
+							<input
+								type="text"
+								placeholder="Photo URL"
+								className="input input-bordered w-full max-w-xs"
+								{...register("img")}
+							/>
+						</div>
+
+						<input
+							className="btn w-full max-w-xs mt-5"
+							type="submit"
+							value="Add Item"
+						/>
+					</form>		
+  			</div>
 			</div>
 		</div>
 	);
